e2043c26b4478d6f771413f9c94169873dd08639
nirbhayjakhar3196 <jakharnirbhay0000@gmail.com>
2026-05-21 22:49:56 +0530

    feat: Can access the data by clicking the button

--- script.js
+++ script.js
@@ -6,10 +6,44 @@ const userAge = document.getElementById('user-age')
 const btn = document.getElementById('btn')
 
 async function getData() {
-    const response = await fetch('https://randomuser.me/api/')
-    const data = await response.json()
 
-    console.log(data);
+    try {
+
+        btn.disabled = true
+        btn.textContent = "loading..."
+
+        userName.textContent = "loading..."
+        const response = await fetch('https://randomuser.me/api/')
+        const data = await response.json()
+
+        console.log(data);
+
+        const user = data.results[0]
+        console.log(user);
+        
+        
+        userImg.src = user.picture.large
+        userName.textContent = `${user.name.first} ${user.name.last}`
+        userEmail.textContent = user.email
+        userCountry.textContent = user.location.country
+        userAge.textContent = `${user.dob.age}
+        `
+        
+        
+    } catch (error) {
+        userName.textContent = "Failed to fetch user.."
+        console.log(error);
+    }
+    finally{
+        btn.disabled = false
+        btn.textContent = "Generate User"
+
+    }
     
 }
+
+
+btn.addEventListener('click', () => {
+    getData()
+})
 getData()
